189baba3d9bf205df29382504c1cf7afdf421ecb
Declan Lubbersen <50627981+dtslubbersen@users.noreply.github.com>
2022-12-09 14:24:19 +0100

    Create mention.py

---
+++ mention/mention.py
@@ -0,0 +1,13 @@
+import discord
+from discord.ext import commands
+
+class UserMention(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def mention(self, ctx):
+        await ctx.send(f"<@!{ctx.thread.id}> (`{ctx.thread.id}`)")
+
+async def setup(bot):
+    await bot.add_cog(UserMention(bot))
